4f4caaa6b5629eba746b1744908edee1a0be7b70
Austin Daspit <adaspit@mediasciencelabs.com>
2019-01-21 10:59:09 -0600

    2nd commit

--- index.py
+++ index.py
@@ -1,6 +1,15 @@
 #Testing Testing
+from difflib import get_close_matches
+
+def closeMatches(patterns, word):
+    if get_close_matches(word, patterns):
+        print('1')
+    else:
+        print('0')
 
 def main():
-    print('Hello')
+    word = 'AT & T'
+    patterns = ['AT&T', 'Pepsi']
+    closeMatches(patterns, word)
 
 main()
